f96a36f2eae6feb169cb5076a1be342ef8559957
ddickyy <dikiidarmawam05x2@gmail.com>
2025-06-11 11:37:37 +0700

    coba

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perpustakaan Cendikia</title>
+</head>
+<style>
+body {
+  background-color: #ffffff;
+}
+
+h1 {
+  color: white;
+  text-align: center;
+}
+
+p {
+  font-family: verdana;
+  font-size: 20px;
+}
+</style>
+<body>
+    <p>Selamat Datang!</p>
+</body>
+</html>
